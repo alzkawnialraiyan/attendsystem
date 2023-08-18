e7537bed7d5accd5a880c3af8ceb8534440f9531
al raiyan al zakwani <alzakwanialraiyan0@gmail.com>
2023-08-18 12:54:46 +0400

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,30 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+        List<Student> studentList = new ArrayList<>();
+
+        Student s1 = new Student();
+        Student s2 = new Student();
+        Student s3 = new Student();
+        Student s4 = new Student();
+        Student s5 = new Student();
+
+        s1.name = "A";
+        s1.name = "B";
+        s1.name = "C";
+        s1.name = "D";
+        s1.name = "E";
+
+        studentList.add(s1);
+        studentList.add(s2);
+        studentList.add(s3);
+        studentList.add(s4);
+        studentList.add(s5);
+
+
+
+
+    }
+}
